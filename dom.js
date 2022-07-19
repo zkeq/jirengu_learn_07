0555f7474bff1e86bf975743a035bbd3513ea354
zkeq <admin@icodeq.com>
2022-07-19 10:25:33 +0800

    todo

--- dom.js
+++ dom.js
@@ -26,3 +26,5 @@ dom.style(div, 'color', 'red') // 设置 div.style.color
 
 const divList = dom.find('.red'); // 获取多个 div.red 元素
 dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
+
+// TODO: https://github.com/Shiqi1998q/DOM-/blob/main/src/dom.js
